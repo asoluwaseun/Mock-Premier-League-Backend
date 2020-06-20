72d0d2c5c998073342f716598fc66ace470f8eb1
Samuel Adesina <adesinsamueloluwaseun@gmail.com>
2020-06-20 22:29:04 +0100

    Created Response Helper

---
+++ helpers/ResponseHelper.js
@@ -0,0 +1,52 @@
+class ResponseHelper{
+    static async sendResponse(
+        res,
+        status,
+        error = false,
+        data = false,
+        message = ""
+    ){
+        let response = {
+            data: data ? data : false,
+            error: error ? true : false
+        }
+
+        switch (status){
+            case 200:
+                response["message"] = message ? message : "Successful";
+                break;
+            case 201:
+                response["message"] = message ? message : "Created Successfully";
+                break;
+            case 203:
+                response["error"] = true;
+                response["message"] = message ? message : "An Error Occured";
+                break;
+            case 401:
+                response["error"] = true;
+                response["message"] = message ? message : "Unauthorized Access";
+                break;
+            case 403:
+                response["error"] = true;
+                response["message"] = message ? message : "Unauthorized Access";
+                break;
+            case 404:
+                response["error"] = true;
+                response["message"] = message ? message : "Resource Not Found";
+                break;
+            case 405:
+                response["error"] = true;
+                response["message"] = message ? message : "Method Not Allowed";
+                break;
+            case 500:
+                response["error"] = true;
+                response["message"] = message ? message : "Internal Server Error";
+                break;
+        }
+
+        return res.status(status).json(response)
+
+    }
+}
+
+module.exports = ResponseHelper;
